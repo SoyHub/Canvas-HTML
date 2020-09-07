51e34898071e3d70b628daf94878b87f6ad77d27
Sohayb Hassan <60256131+SoyHub@users.noreply.github.com>
2020-09-07 14:35:40 +0200

    onload image

--- Change Canvas Imgaes/create.js
+++ Change Canvas Imgaes/create.js
@@ -1,18 +1,27 @@
 // Code goes here
 var stage;
+
 function init() {
   var canvas = document.getElementById("canvas");
   stage = new createjs.Stage(canvas);
 
-  createImage("https://images.unsplash.com/photo-1596785231809-a90a89c0ef47?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max");
-  createImage("https://images.unsplash.com/photo-1597679441957-233da416c807?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max");
-  createImage("https://source.unsplash.com/random");
+  AddImage("./kitarou.png");
+  AddImage("./1.png");
+  AddImage("./2.png");
+  AddImage("./3.png");
 
   //Update stage will render next frame
   stage.update();
 }
+function AddImage(src) {
+  var image = new Image();
+  image.src = src;
+  image.crossOrigin = "Anonymous";
+  image.onload = handleImageLoad;
+  stage.update();
+}
 
-function allowDrag(item){
+function allowDrag(item) {
   item.addEventListener("mousedown", function (event) {
     var sX = Math.floor(event.stageX);
     var sY = Math.floor(event.stageY);
@@ -36,21 +45,30 @@ function allowDrag(item){
   });
 }
 
-function createImage(src) {
-  var image = new Image();
-  image.crossOrigin = "Anonymous";
-  image.src= src;
-  
-  //Create image
-  var img = new createjs.Bitmap(image);
-  //Set position of Shape instance.
-  img.x = img.y = 20;
-  img.scaleX=0.1;
-  img.scaleY=0.1;
+function handleFileLoad(event) {
+  var item = event.item; // A reference to the item that was passed in to the LoadQueue
+  var type = item.type;
+
+  // Add any images to the page body.
+  if (type == createjs.Types.IMAGE) {
+    document.body.appendChild(event.result);
+  }
+}
+
+function handleImageLoad(event) {
+  var image = event.target;
+  var bitmap = new createjs.Bitmap(image);
+ 
+  //place on screen
+  bitmap.x = bitmap.y = 200;
+
+  //size of item
+  bitmap.scaleX = 1;
+  bitmap.scaleY = 1;
 
   //AllowDrag
-  allowDrag(img);
-
-  stage.addChild(img);
-
+  allowDrag(bitmap);
+  stage.addChild(bitmap);
+  stage.update();
 }
+
